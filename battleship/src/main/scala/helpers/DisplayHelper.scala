15b4e1090c855aa02c7c00a6780aa77a81cccb01
Unknown <fabien.turgut@etu.umontpellier.fr>
2018-10-03 16:16:48 +0200

    display sent shots grid

--- battleship/src/main/scala/helpers/DisplayHelper.scala
+++ battleship/src/main/scala/helpers/DisplayHelper.scala
@@ -49,40 +49,46 @@ object DisplayHelper {
         val gridSize: Int = GameSettings.gridSize
         val boatCells: List[Cell] = player.boats.flatMap((b) => b.cells)
 
-        println(player.name + " boat grid :")
-        def displayGrid(x: Int, y: Int, gridSize: Int, boatCells: List[Cell]): Unit = {
+        def displayGrid(x: Int, y: Int, gridSize: Int, boatCells: List[Cell], shots: List[Shot]): Unit = {
             //Numbers on top
             if(x <= gridSize && y == gridSize+1){
                 print(" " + x)
-                displayGrid(x+1, y, gridSize, boatCells)
+                displayGrid(x+1, y, gridSize, boatCells, shots)
             }
             //Last top Cell
             else if(x == gridSize+1 && y == gridSize+1){
                 println()
-                displayGrid(1, y-1, gridSize, boatCells)
+                displayGrid(1, y-1, gridSize, boatCells, shots)
             }
             //Normal Cell
             else if(x <= gridSize && y > 0){
                 val filteredCells: List[Cell] =  boatCells.filter((s) => s.x == x && s.y == y)
-                val filteredShots: List[Shot] =  player.receivedShots.filter((rs) => rs.x == x && rs.y == y)
+                val filteredShots: List[Shot] =  shots.filter((rs) => rs.x == x && rs.y == y)
                 if(filteredCells.length > 0){
                     val cell: Cell = filteredCells.head
                     print("|" + cell.state)
                 }
                 else if(filteredShots.length > 0){
-                    print("|" + GameSettings.untouchedDisplay)
+                    if(filteredShots.head.touched) 
+                        print("|" + GameSettings.touchedDisplay)
+                    else 
+                        print("|" + GameSettings.untouchedDisplay)
                 }
                 else{
                     print("| ")
                 }
-                displayGrid(x+1, y, gridSize, boatCells)
+                displayGrid(x+1, y, gridSize, boatCells, shots)
             }
             //Numbers on right
             else if(x >= gridSize && y > 0 && y < gridSize+1){
                 println("| " + y)
-                displayGrid(1, y-1, gridSize, boatCells)
+                displayGrid(1, y-1, gridSize, boatCells, shots)
             }
         }
-        displayGrid(1, gridSize+1, gridSize, boatCells)
+
+        println(player.name + " boat grid :")
+        displayGrid(1, gridSize+1, gridSize, boatCells, player.receivedShots)
+        println(player.name + " shots grid :")
+        displayGrid(1, gridSize+1, gridSize, List(), player.sentShots)
     }
 }
