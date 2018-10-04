25bd0c7df28fa32a0f7d9f82503efb346a151c45
Unknown <fabien.turgut@etu.umontpellier.fr>
2018-10-04 16:06:16 +0200

    change Shot class

--- battleship/src/main/scala/players/Shot.scala
+++ battleship/src/main/scala/players/Shot.scala
@@ -1,8 +1,22 @@
 package players
 
-case class Shot(val x: Int, val y: Int, val touched: Boolean){
-    def setTouched(touched: Boolean): Shot = {
-        new Shot(x, y, touched)
+case class Shot(
+    val x: Int, 
+    val y: Int, 
+    val touched: Boolean = false, 
+    val boatSize: Int = 0, 
+    val sankBoat: Boolean = false
+){
+    def setTouched(newTouched: Boolean): Shot = {
+        Shot(x, y, newTouched, boatSize, sankBoat)
+    }
+
+    def setBoatSize(newBoatSize: Int): Shot = {
+        Shot(x, y, touched, newBoatSize, sankBoat)
+    }
+
+    def setSankBoat(newSankBoat: Boolean): Shot = {
+        Shot(x, y, touched, boatSize, newSankBoat)
     }
 }
 
