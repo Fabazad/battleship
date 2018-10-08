e34056eacef61c245a159106a2c7a8636d7aa556
Unknown <fabien.turgut@etu.umontpellier.fr>
2018-10-08 16:17:10 +0200

    Revert "special contest"
    This reverts commit f452d34a360007b5c4801aa26fea3f542654c151.

--- battleship/src/main/scala/game/GameSettings.scala
+++ battleship/src/main/scala/game/GameSettings.scala
@@ -5,5 +5,5 @@ object GameSettings {
     val boats: List[Int] = List(5,4,3,3,2)
     val touchedDisplay: String = "\u001b[32mo\u001b[0m"
     val untouchedDisplay: String = "\u001b[31mo\u001b[0m"
-    val contestGames: Int = 10000
+    val contestGames: Int = 100
 }
